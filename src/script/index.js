23ae4963e3dc2936ddc5b24761f5a85b63bd3fa2
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-03-05 15:05:34 -0300

    Sessão de produtos dinâmica

--- src/script/index.js
+++ src/script/index.js
@@ -26,3 +26,42 @@ function asideCtrl(){
         hideScreen.remove("hide-screen");
     }
 }
+
+async function loadCards(){
+    const url = "../../materials.json";
+
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Erro HTTP. Status ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        const carousel = document.querySelector(".carousel");
+        const template = document.getElementById("product_card_template");
+
+        data.forEach(e => {
+            const clone = template.content.cloneNode(true);
+
+            const card = clone.querySelector(".card");
+            card.addEventListener("click", function () {
+                sendMessage(e.title);
+            });
+
+            clone.querySelector(".img").src = `../../assets/image/materials/${e.img}`;
+            clone.querySelector(".img").alt = e.title;
+            clone.querySelector(".type").classList.add(e.type);
+            clone.querySelector(".type").innerHTML = e.type == "tool" ? "Ferramenta" : "Material";
+            clone.querySelector(".title").innerHTML = e.title;
+            clone.querySelector(".subtitle").innerHTML = e.subtitle;
+
+            carousel.appendChild(clone);
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+loadCards();
